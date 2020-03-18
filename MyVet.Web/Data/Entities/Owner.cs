9d73eb34b34b6c1a1bf37572b2ae7153c25a4043
clanhustveg@gmail.com <clanhustveg@gmail.com>
2020-03-17 20:35:39 -0600

    DataContext Entities Owner Migrations cree base de datos y la conexión local a la base de datos sql server

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+
+
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength (30)]
+        public string  Document { get; set; }
+
+        [Required]
+        [MaxLength (150)]
+        
+        [Display (Name= "Nombre"  )]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+
+        [MaxLength(8)]
+        [Display(Name = "Teléfono")]
+        public string FixedPhone { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        [Display(Name = "Celular")]
+        public string Cellphone { get; set; }
+
+        [MaxLength(150)]
+        [Display(Name = "Dirección")]
+        public string Address { get; set; }
+
+        [Display(Name = "Propietario")]
+        public string FullName => $"{FirstName}{LastName}";
+        [Display(Name = "Propietario")]
+        public string FullNameWithDocument => $"{FirstName}{LastName}-{Document}";
+    }
+}
---
+++ MyVet.Web/Migrations/20200318022346_initialDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyVet.Web.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Document = table.Column<string>(maxLength: 30, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 150, nullable: false),
+                    LastName = table.Column<string>(maxLength: 150, nullable: false),
+                    FixedPhone = table.Column<string>(maxLength: 8, nullable: true),
+                    Cellphone = table.Column<string>(maxLength: 8, nullable: false),
+                    Address = table.Column<string>(maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
